88aca8a7f0922b2cdc54e46c2a6da66e54f206cd
PBOSSDEV <piluncp@gmail.com>
2024-11-28 17:21:29 +0700

    Edit taskpage cccc

--- src/views/TaskPage.vue
+++ src/views/TaskPage.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="task-page">
-      <h1 style="color: black;">Welcome to Task Pages okk Boss krub
+      <h1 style="color: black;">Welcome to Task Pages okkk
       </h1>
     </div>
   </template>
